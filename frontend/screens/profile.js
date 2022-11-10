eb913b4e236dcc36af9dbac95169a7f0e5623581
chaujono <chaujono@users.noreply.github.com>
2022-11-10 20:50:09 +1100

    added page stubs for home, profile and sell, added bottom navigation bar

---
+++ frontend/screens/profile.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+
+
+export default function ProfileScreen({ navigation }) {
+
+    return (
+        <View>
+
+        </View>
+    );
+
+}
