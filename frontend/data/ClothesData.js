35d354dc58c01104a6fda9ba4cde28198774aae6
MComino <68498524+MComino@users.noreply.github.com>
2022-11-11 15:53:14 +1100

    creating clothes data and adjusting the navigation to allow for changing between different screens

---
+++ frontend/data/ClothesData.js
@@ -0,0 +1,70 @@
+const clothing_items = [
+    {
+        "id": 1,
+        "title": "Green Hoodie",
+        "type": "Thriftstore",
+        "price": "5",
+        "description": "The greener the shirt the more environmentally conscious you are right?",
+        "size": "M",
+        "condition": "Good",
+        "location": "Joe's Thriftstore",
+        "image": "../images/green_hoodie.png"
+    },
+    {
+        "id": 2,
+        "title": "Grey Dress",
+        "type": "Warehouse",
+        "price": "2",
+        "description": "The greyer the dress the better right?",
+        "size": "S",
+        "condition": "Good",
+        "location": "We can come to you or you can drop it off to us. Your choice!",
+        "image": "../images/grey_dress.png"
+    },
+    {
+        "id": 3,
+        "title": "Black Jacket",
+        "type": "Warehouse",
+        "price": "7",
+        "description": "The blacker the jacket the cooler you are right?",
+        "size": "L",
+        "condition": "Poor",
+        "location": "We can come to you or you can drop it off to us. Your choice!",
+        "image": "../images/black_jacket.png"
+    },
+    {
+        "id": 4,
+        "title": "Red Dress",
+        "type": "Thriftstore",
+        "price": "8",
+        "description": "I'm a fairy princess",
+        "size": "M",
+        "condition": "Great",
+        "location": "Samuel's Thriftstore",
+        "image": "../images/red_dress.png"
+    },
+    {
+        "id": 5,
+        "title": "Belt",
+        "type": "Thriftstore",
+        "price": "5",
+        "description": "A belt to hold up  your pants",
+        "size": "XL",
+        "condition": "Great",
+        "location": "Joe's Thriftstore",
+        "image": "../images/belt.png"
+    },
+    {
+        "id": 6,
+        "title": "Black Shoes",
+        "type": "Warehouse",
+        "price": "7",
+        "description": "Some pretty high heels",
+        "size": "S",
+        "condition": "Poor",
+        "location": "We can come to you or you can drop it off to us. Your choice!",
+        "image": "../images/black_shoes.png"
+    }
+]
+
+export default clothing_items
